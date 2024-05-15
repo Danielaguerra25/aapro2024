e55329dea59bda499e691f7d1e1f5860b1e378f4
Danielaguerra25 <154599960+Danielaguerra25@users.noreply.github.com>
2024-05-15 20:14:11 -0300

    Add files via upload
    envio ejercicio numero 10 con JavaScript

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+var texto= texto;
+var nombre= " daniela ";
+var apellido= " guerra ";
+ var texto= nombre+ apellido;
+console.log ("bienvenida" +  texto )
+
+
+var num=37;
+var numero_2=89;
+var suma= num + numero_2;
+console.log ("la suma es  " + suma)
+
+var numero_3 = 567;
+ var suma2=  suma + numero_3;
+ console.log (" la suma es " + suma2)
